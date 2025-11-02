9a32fe69eb06998f86bd7ab095899e7e14f8731c
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-02 15:46:34 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 
 
@@ -12,6 +13,8 @@ const App = () => {
   return (
    <main>
     <Navbar/>
+    <Hero/>
+    <div className="h-dvh bg-black"></div>
    </main>
   )
 }
